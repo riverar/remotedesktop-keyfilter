9a5a575090b18e703eb76f086924073b002d039e
Rafael Rivera <rafael@withinwindows.com>
2015-10-27 10:00:24 -0700

    Removed pdb dep

--- remotedesktop-keyfilter.js
+++ remotedesktop-keyfilter.js
@@ -1,138 +1,146 @@
 (function () {
-    var keyboardProc = DebugSymbol.getFunctionByName("CTSInput::IHStaticLowLevelKeyboardProc");
-    var originalKeyboardProc = new NativeFunction(keyboardProc, 'int64', ['int', 'uint64', 'pointer'], 'win64');
 
-    var CallNextHookEx = new NativeFunction(Module.findExportByName("user32.dll", "CallNextHookEx"), 'long', ['pointer', 'int', 'uint64', 'pointer']);
-    var keybd_event = new NativeFunction(Module.findExportByName("user32.dll", "keybd_event"), 'void', ['uchar', 'uchar', 'int32', 'uint64']);
+    Memory.scan(Module.findBaseAddress("mstscax.dll"), 500000, "48 89 5C 24 08 48 89 74 24 10 57 48 83 EC 20 8B F1", {
+      onMatch: (addr, size) => { keyboardProc = addr; return "stop"; },
+      onComplete: () => { InstallHook(keyboardProc); }
+    });
+  
+    function InstallHook(keyboardProcAddress) {
+      var originalKeyboardProc = new NativeFunction(keyboardProcAddress, 'int64', ['int', 'uint64', 'pointer'], 'win64');
 
-    var WM_KEYDOWN = 256;
-    var WM_KEYUP = 257;
+      var CallNextHookEx = new NativeFunction(Module.findExportByName("user32.dll", "CallNextHookEx"), 'long', ['pointer', 'int', 'uint64', 'pointer']);
+      var keybd_event = new NativeFunction(Module.findExportByName("user32.dll", "keybd_event"), 'void', ['uchar', 'uchar', 'int32', 'uint64']);
 
-    var VK_LEFT = 37;
-    var VK_RIGHT = 39;
-    var VK_LWIN = 91;
-    var VK_RWIN = 92;
-    var VK_LCONTROL = 162;
-    var VK_RCONTROL = 163;
+      var WM_KEYDOWN = 256;
+      var WM_KEYUP = 257;
 
-    var PASS_THROUGH_HOOK = 0;
-    var SKIP_HOOK = 1;
+      var VK_LEFT = 37;
+      var VK_RIGHT = 39;
+      var VK_LWIN = 91;
+      var VK_RWIN = 92;
+      var VK_LCONTROL = 162;
+      var VK_RCONTROL = 163;
 
-    function InjectKeys(keys, state) {
-      keys.every((key) => {
-          keybd_event(key, 0, state, 0xfeed);
-      });
-    }
+      var PASS_THROUGH_HOOK = 0;
+      var SKIP_HOOK = 1;
 
-    this.__sequences = [
-      [VK_LCONTROL, VK_LWIN, VK_LEFT],
-      [VK_LCONTROL, VK_LWIN, VK_RIGHT],
-      [VK_LWIN, VK_LCONTROL, VK_LEFT],
-      [VK_LWIN, VK_LCONTROL, VK_RIGHT],
-    ];
-
-    var MatchType = { None: 0, Partial: 1, Full: 2 };
-
-    function EvaluateQueue() {
-      var sequences = this.__sequences;
-      var queue = this.__queue;
-
-      var result = { matchType: MatchType.None, sequence: -1 };
-
-      for (var si = 0; si < sequences.length; si++) {
-          var sequence = sequences[si];
-
-          var numOfElemToCheck = (queue.length < sequence.length) ? queue.length : sequence.length;
-
-          var matchFound = true;
-          for (var i = 0; i < numOfElemToCheck; i++) {
-              if (queue[i] != sequence[i]) {
-                  matchFound = false;
-                  break;
-              }
-          }
-
-          if (matchFound) {
-              result.matchType = (queue.length == sequence.length) ? MatchType.Full : MatchType.Partial;
-              result.sequence = si;
-
-              if (result.matchType == MatchType.Full)
-                  return result;
-          }
+      function InjectKeys(keys, state) {
+        keys.every((key) => {
+            keybd_event(key, 0, state, 0xfeed);
+        });
       }
 
-      return result;
-    }
+      this.__sequences = [
+        [VK_LCONTROL, VK_LWIN, VK_LEFT],
+        [VK_LCONTROL, VK_LWIN, VK_RIGHT],
+        [VK_LWIN, VK_LCONTROL, VK_LEFT],
+        [VK_LWIN, VK_LCONTROL, VK_RIGHT],
+      ];
 
-    function OnKeyDown(context) {
-      if (context.extra == 0xfeed)
-          return PASS_THROUGH_HOOK;
+      var MatchType = { None: 0, Partial: 1, Full: 2 };
 
-      if (context.vkCode == 255) // RDP-specific sync message
-          return PASS_THROUGH_HOOK;
+      function EvaluateQueue() {
+        var sequences = this.__sequences;
+        var queue = this.__queue;
 
-      if (this.__queue[this.__queue.length - 1] != context.vkCode) {
-          this.__queue.push(context.vkCode);
+        var result = { matchType: MatchType.None, sequence: -1 };
+
+        for (var si = 0; si < sequences.length; si++) {
+            var sequence = sequences[si];
+
+            var numOfElemToCheck = (queue.length < sequence.length) ? queue.length : sequence.length;
+
+            var matchFound = true;
+            for (var i = 0; i < numOfElemToCheck; i++) {
+                if (queue[i] != sequence[i]) {
+                    matchFound = false;
+                    break;
+                }
+            }
+
+            if (matchFound) {
+                result.matchType = (queue.length == sequence.length) ? MatchType.Full : MatchType.Partial;
+                result.sequence = si;
+
+                if (result.matchType == MatchType.Full)
+                    return result;
+            }
+        }
+
+        return result;
       }
 
-      var result = EvaluateQueue();
+      function OnKeyDown(context) {
+        if (context.extra == 0xfeed)
+            return PASS_THROUGH_HOOK;
 
-      if (result.matchType == MatchType.None) {
-          setTimeout(() => { InjectKeys(this.__queue.slice(0), 2) }, 0);
-          this.__queue.length = 0;
-          return PASS_THROUGH_HOOK;
-      } else if (result.matchType == MatchType.Partial) {
-          return SKIP_HOOK;
-      } else {
-          this.__queue.pop();
-          return SKIP_HOOK;
-      }
-    }
+        if (context.vkCode == 255) // RDP-specific sync message
+            return PASS_THROUGH_HOOK;
 
-    function OnKeyUp(context) {
-      for (var i = 0; i < this.__queue.length; i++) {
-          if (this.__queue[i] === context.vkCode) {
-              this.__queue.splice(i, 1);
-          }
+        if (this.__queue[this.__queue.length - 1] != context.vkCode) {
+            this.__queue.push(context.vkCode);
+        }
+
+        var result = EvaluateQueue();
+
+        if (result.matchType == MatchType.None) {
+            setTimeout(() => { InjectKeys(this.__queue.slice(0), 2) }, 0);
+            this.__queue.length = 0;
+            return PASS_THROUGH_HOOK;
+        } else if (result.matchType == MatchType.Partial) {
+            return SKIP_HOOK;
+        } else {
+            this.__queue.pop();
+            return SKIP_HOOK;
+        }
       }
+
+      function OnKeyUp(context) {
+        for (var i = 0; i < this.__queue.length; i++) {
+            if (this.__queue[i] === context.vkCode) {
+                this.__queue.splice(i, 1);
+            }
+        }
+        
+        return PASS_THROUGH_HOOK;
+      }
+
+      function GetContext(nCode, wParam, lParam) {
+        if (typeof this.__queue == 'undefined')
+            this.__queue = [];
+            
+        return {
+            shouldProcess: (nCode == 0),
+            message: wParam,
+            vkCode: Memory.readInt(lParam),
+            scanCode: Memory.readInt(lParam.add(4)),
+            flags: Memory.readInt(lParam.add(8)),
+            time: Memory.readInt(lParam.add(12)),
+            extra: Memory.readInt(lParam.add(16)),
+            queue: this.__queue
+        }
+      }
+
+      Interceptor.replace(keyboardProcAddress, new NativeCallback(function (nCode, wParam, lParam) {
+        var context = GetContext(nCode, wParam, lParam);
+        var returnImmediately = false;
+        
+        switch (context.message) {
+            case WM_KEYDOWN:
+                returnImmediately = OnKeyDown(context);
+                break;
+
+            case WM_KEYUP:
+                returnImmediately = OnKeyUp(context);
+                break;
+        }
+
+        if (returnImmediately) {
+            return CallNextHookEx(NULL, 0, wParam, lParam);
+        } else {
+            return originalKeyboardProc(nCode, wParam, lParam);
+        }
+      }, 'int64', ['int', 'uint64', 'pointer']));
       
-      return PASS_THROUGH_HOOK;
     }
-
-    function GetContext(nCode, wParam, lParam) {
-      if (typeof this.__queue == 'undefined')
-          this.__queue = [];
-          
-      return {
-          shouldProcess: (nCode == 0),
-          message: wParam,
-          vkCode: Memory.readInt(lParam),
-          scanCode: Memory.readInt(lParam.add(4)),
-          flags: Memory.readInt(lParam.add(8)),
-          time: Memory.readInt(lParam.add(12)),
-          extra: Memory.readInt(lParam.add(16)),
-          queue: this.__queue
-      }
-    }
-
-    Interceptor.replace(keyboardProc, new NativeCallback(function (nCode, wParam, lParam) {
-      var context = GetContext(nCode, wParam, lParam);
-      var returnImmediately = false;
-      
-      switch (context.message) {
-          case WM_KEYDOWN:
-              returnImmediately = OnKeyDown(context);
-              break;
-
-          case WM_KEYUP:
-              returnImmediately = OnKeyUp(context);
-              break;
-      }
-
-      if (returnImmediately) {
-          return CallNextHookEx(NULL, 0, wParam, lParam);
-      } else {
-          return originalKeyboardProc(nCode, wParam, lParam);
-      }
-    }, 'int64', ['int', 'uint64', 'pointer']));
 }());
